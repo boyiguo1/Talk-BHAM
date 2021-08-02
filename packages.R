bbc875058819b204fe904bd9c5c91d00d37618ef
Boyi Guo <boyiguo1@uab.edu>
2021-08-02 15:06:28 -0500

    Start to make slides

---
+++ packages.R
@@ -0,0 +1,13 @@
+## library() calls go here
+library(conflicted)
+library(dotenv)
+library(targets)
+library(tarchetypes)
+
+
+
+library(officedown)
+library(officer)
+library(tidyverse)
+library(knitr)
+library(flextable)
